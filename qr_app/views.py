78a005c88d3b553b824e01efe3a9dcfdfad63ee1
arafat_ratul <yeasirarafat.ratul@gmail.com>
2020-09-25 12:36:22 +0600

    without-template

--- qr_app/views.py
+++ qr_app/views.py
@@ -2,11 +2,4 @@ from django.shortcuts import render
 from .models import QR
 # Create your views here.
 
-def home(request):
-    obj = QR.objects.all()
-
-    context = {
-        'obj':obj,
-    }
-    return render(request,'home.html',context)
 
--- qr_project/urls.py
+++ qr_project/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from qr_app.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+   
 ]
